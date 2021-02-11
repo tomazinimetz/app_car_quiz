db59cdaba6b25b6cd6dca14ca1653e7cf22f6399
Mateus Metz <tomazinimetz@gmail.com>
2021-02-11 10:26:56 -0300

    create assets file

--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -1,3 +1,9 @@
 class Assets{
+  static String get bmwLogo => "assets/bmw_logo.png";
+  static String get toyotaLogo => "assets/toyota_logo.png";
+  static String get generalMotorsLogo => "assets/general_motors_logo.png";
+  static String get miniLogo => "assets/mini_logo.png";
+  static String get rollsRoyceLogo => "assets/rolls_royce_logo.png";
+
   static String get splash => "assets/splash_icon.jpg";
 }
